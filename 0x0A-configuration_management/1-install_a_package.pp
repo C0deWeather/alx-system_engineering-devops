df273d9b8ce66259792389fa90c5b0e07b6e4ec8
C0deWeather <frankbright211@gmail.com>
2024-07-14 20:51:42 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # This Puppet script installs flask from pip3
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/pip3 show Flask | grep "Version: 2.1.0"',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
